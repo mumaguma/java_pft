5f08665045d1db8c276d970beedf72dff66ee7ca
mumaguma <148da70e@opayq.com>
2018-07-02 17:09:38 +0200

    wprowadzone zmienne

--- sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,9 @@ package pl.stqa.pft.sandbox;
 public class MyFirstProgram {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+    int l = 16;
+
+    System.out.println("kwadrat o boku " + l + " ma pole " + l*l);
   }
 }
 
