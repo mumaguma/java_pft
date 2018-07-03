fb8f58a7ded1cb7b587cb89cb5ce53c093769e77
mumaguma <148da70e@opayq.com>
2018-07-03 10:15:00 +0200

    Dodane funkcje mnożenia powierzchni

--- sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,11 +2,24 @@ package pl.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
-    int l = 16;
+    hello("ssss");
+    hello("aaaa");
+    double v = 5;
+    double x = 4;
+    System.out.println("Powierzchnia o boku " + v + " = " + powierzchnia(v));
+    System.out.println("Powierzchnia o bokach " + v + ", " + x + " = " + powierzchnia(v, x));
 
-    System.out.println("kwadrat o boku " + l + " ma pole " + l*l);
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double powierzchnia(double krawedz) {
+    return krawedz * krawedz;
+  }
+
+  public static double powierzchnia(double a, double b ) {
+    return a * b;
   }
 }
-
- 
