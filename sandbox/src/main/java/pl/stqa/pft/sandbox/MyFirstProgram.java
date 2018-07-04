bc6d91974ec0479df163c14572119be4dd572c8e
mumaguma <148da70e@opayq.com>
2018-07-04 16:01:47 +0200

    początek pracy domowej

--- sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,23 +2,16 @@ package pl.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    hello("ssss");
-    hello("aaaa");
 
-    Square s = new Square(5);
-    Rectangle r = new Rectangle(7, 8);
-    Rectangle klasycznyobiekt = new Rectangle(12, 92);
-
-    System.out.println("Powierzchnia o boku " + s.l + " = " + s.powierzchnia());
-    System.out.println("Powierzchnia o bokach " + r.x + ", " + r.y + " = " + r.powierzchnia());
-    System.out.println("potwierzenie dla " + klasycznyobiekt.x + " oraz " + klasycznyobiekt.y + " to " + klasycznyobiekt.powierzchnia());
-
+    Point p1 = new Point (0,0);
+    Point p2 = new Point (10,10);
   }
 
-  public static void hello(String somebody) {
-    System.out.println("Hello " + somebody + "!");
-  }
 
+  public static double distance(Point p1, Point p2){
+        double x =
+
+  }
 }
 
 
--- sandbox/src/main/java/pl/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/Point.java
@@ -3,4 +3,11 @@ package pl.stqa.pft.sandbox;
 public class Point {
   public double x;
   public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+
 }
