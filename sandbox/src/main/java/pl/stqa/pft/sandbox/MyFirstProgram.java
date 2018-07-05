df1a854b4bd4e4cfcc237ac394b6cafdeae3a9b9
mumaguma <148da70e@opayq.com>
2018-07-05 09:50:50 +0200

    Praca domowa 2 - wersja z funkcją

--- sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,14 +3,13 @@ package pl.stqa.pft.sandbox;
 public class MyFirstProgram {
   public static void main(String[] args) {
 
-    Point p1 = new Point (0,0);
-    Point p2 = new Point (10,10);
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(5, 5);
+    System.out.println("Odleglosc punktow to " + distance(p1, p2 ));
   }
 
-
-  public static double distance(Point p1, Point p2){
-        double x =
-
+  public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p1.x + p2.x)*(p1.x + p2.x)+(p1.y + p2.y)*(p1.y + p2.y));
   }
 }
 
