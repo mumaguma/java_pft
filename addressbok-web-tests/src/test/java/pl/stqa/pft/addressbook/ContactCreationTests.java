136fdcb85bfb1de197e1d8fd9542fec0db26c8cb
mumaguma <148da70e@opayq.com>
2018-07-10 16:20:12 +0200

    parametryzacja wprowadzona do ContactCreationTests

--- addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/ContactCreationTests.java
@@ -17,24 +17,24 @@ public class ContactCreationTests {
   public void setUp() throws Exception {
     wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Users/dwi/Dropbox (QualityMinds GmbH)/_dwi/learning/_Java dla testerow/FirefoxESR/App/firefox64/Firefox.exe"));
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    login();
+    login("admin", "secret");
   }
 
-  private void login() {
+  private void login(String username, String password) {
     wd.get("http://localhost/addressbook/");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
   @Test
   public void testContactCreation() {
     initContactCreation();
-    fillContactForm();
+    fillContactForm("Adam", "Adamowicz", "ul. Prosta 1\n00-001 Warszawa", "+48 123 456 789", "adam@wp.pl");
     submitContactCreation();
     returnToMainPage();
   }
@@ -47,23 +47,23 @@ public class ContactCreationTests {
     wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
-  private void fillContactForm() {
+  private void fillContactForm(String firstName, String lastName, String streetAddress, String phoneHome, String email) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Adam");
+    wd.findElement(By.name("firstname")).sendKeys(firstName);
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Adamowicz");
+    wd.findElement(By.name("lastname")).sendKeys(lastName);
     wd.findElement(By.name("nickname")).click();
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("ul. Prosta 1\n00-001 Warszawa");
+    wd.findElement(By.name("address")).sendKeys(streetAddress);
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("+48 123 456 789");
+    wd.findElement(By.name("home")).sendKeys(phoneHome);
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("adam@wp.pl");
+    wd.findElement(By.name("email")).sendKeys(email);
   }
 
   private void initContactCreation() {
