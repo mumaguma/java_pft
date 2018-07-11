6bdb86855cc8b9142b815af4bf8f2371a3ae5f88
mumaguma <148da70e@opayq.com>
2018-07-11 03:11:00 +0200

    TestBase created, GroupDeletion created

---
+++ addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/GroupDeletionTest.java
@@ -0,0 +1,17 @@
+package pl.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTest  extends TestBase {
+
+
+  @Test
+  public void testGroupDeletion() {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    gotoGroupPage();
+  }
+
+
+}
