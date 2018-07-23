c56bc2042513638c1be0bfb45c2d186cfe1f7b15
mumaguma <148da70e@opayq.com>
2018-07-23 12:08:59 +0200

    Lazy fields in GroupModificationTests

--- addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/BaseHelper.java
+++ addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/BaseHelper.java
@@ -18,12 +18,15 @@ public class BaseHelper {
   protected void type(By locator, String text) {
     click(locator);
     if (text != null) {
-      wd.findElement(locator).clear();
-      wd.findElement(locator).sendKeys(text);
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if (!text.equals(existingText)) {
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
     }
   }
 
-  protected void acceptbutton(){
+  protected void acceptbutton() {
     wd.switchTo().alert().accept();
   }
 
