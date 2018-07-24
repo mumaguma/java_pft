3ed6c22779ef5041ec052cf40565fcb3746f5e57
mumaguma <148da70e@opayq.com>
2018-07-24 12:47:23 +0200

    optimised page transitions

--- addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -3,17 +3,25 @@ package pl.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper extends BaseHelper{
+public class NavigationHelper extends BaseHelper {
 
   public NavigationHelper(WebDriver wd) {
     super(wd);
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))){
+      return;
+    }
     click(By.linkText("groups"));
   }
 
   public void gotoContactPage() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
     click(By.linkText("home"));
   }
 }
