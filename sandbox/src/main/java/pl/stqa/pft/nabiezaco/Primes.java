1369d88e75a750b28d53af1843735444b243801e
mumaguma <148da70e@opayq.com>
2018-07-27 14:00:55 +0200

    Primes class created

---
+++ sandbox/src/main/java/pl/stqa/pft/nabiezaco/Primes.java
@@ -0,0 +1,34 @@
+package pl.stqa.pft.nabiezaco;
+
+public class Primes {
+
+  public static boolean isPrime(int n) {
+    int sqr = (int) Math.sqrt(n);
+    for (int i = 2; i <= sqr; i++ ){
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean isPrime(long n) {
+    int sqr = (int) Math.sqrt(n);
+    for (long i = 2; i <= sqr; i++ ){
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static boolean isPrimeWhile(int n) {
+    double sqr = Math.sqrt(n);
+    int i = 2;
+    while (i <= sqr && n % i != 0) {
+      i++;
+    }
+    System.out.println(i + "   " + n + "    " + sqr);
+    return i > sqr;
+  }
+}
---
+++ sandbox/src/test/java/pl/stqa/pft/nabiezaco/PrimeTests.java
@@ -0,0 +1,28 @@
+package pl.stqa.pft.nabiezaco;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pl.stqa.pft.nabiezaco.Primes;
+
+public class PrimeTests {
+
+  @Test
+  public void testPrime(){
+    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+//    Assert.assertTrue(Primes.isPrimeWhile(Integer.MAX_VALUE ));
+  }
+
+
+  @Test
+  public void testPrimeLong(){
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+
+  @Test(enabled = false)
+  public void testNonPrime(){
+    Assert.assertFalse(Primes.isPrime(Long.MAX_VALUE -2));
+//    Assert.assertFalse(Primes.isPrimeWhile(Integer.MAX_VALUE -2));
+  }
+
+}
