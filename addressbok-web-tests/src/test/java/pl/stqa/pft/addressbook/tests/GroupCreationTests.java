cc64d4b869570511cf8896589d2f864ac3e8415c
mumaguma <148da70e@opayq.com>
2018-08-02 13:02:20 +0200

    group lists introduced to group tests

--- addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,16 +3,19 @@ package pl.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.GroupData;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+//    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, (before + 1));
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+//    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after.size(), before.size() + 1);
   }
 
 }
