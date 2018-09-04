477ef158a3adaaafc2bb7e1c91e2d1c7adc8a5b0
mumaguma <148da70e@opayq.com>
2018-09-05 01:41:04 +0200

    caching and hashing for groups

--- addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -24,8 +24,8 @@ public class GroupModificationTests extends TestBase {
     GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData().withId(modifiedGroup.getId()).withName("test1x").withHeader("test2x").withFooter("test3x");
     app.group().modify(group);
+    assertEquals(app.group().count(), before.size());
     Groups after = app.group().all();
-    assertEquals(after.size(), before.size());
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
 }
