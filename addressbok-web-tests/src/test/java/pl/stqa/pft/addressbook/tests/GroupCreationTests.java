dcd80e1dda8c5398cde6ce231676bd0a24b2b1d3
mumaguma <148da70e@opayq.com>
2018-09-18 10:13:20 +0200

    writing and saving XML introduced

--- addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbok-web-tests/src/test/java/pl/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,13 +1,18 @@
 package pl.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pl.stqa.pft.addressbook.model.GroupData;
 import pl.stqa.pft.addressbook.model.Groups;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,17 +20,24 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() {
-    List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[] {"test1", "head1", "foo1"});
-    list.add(new Object[] {"test2", "head2", "foo2"});
-    list.add(new Object[] {"test3", "head3", "foo3"});
-    return list.iterator();
+  public Iterator<Object[]> validGroups() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null) {
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xstream = new XStream();
+    XStream.setupDefaultSecurity(xstream);
+    xstream.allowTypes(new Class[]{GroupData.class});
+    xstream.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+    return groups.stream().map((g) -> new Object[] {g} ).collect(Collectors.toList()).iterator();
   }
 
   @Test (dataProvider = "validGroups")
-  public void testGroupCreation(String name, String header, String footer) {
-    GroupData group = new GroupData().withName(name).withHeader(header).withFooter(footer);
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
     app.group().create(group);
